77fa05613cf48c1413c74ac2c1e0ac479caa1a2b
bamwannipaa <66309010018@thonburi.ac.th>
2024-06-13 09:20:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function cal() {
+    let number1 = document.getElementById("number1");
+    let result = document.getElementById("result");
+    let sum = parseFloat(number1.value) * 590.52 ; 
+    result.innerHTML = sum ;
+}
